09fce393548a9a9b4ec8ed865be43f0ee0a55a3f
mic-f <michal.frej@interia.pl>
2015-03-22 21:52:38 +0100

    I modified the cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+### The following functions checks whether two matrices are the same
+matequal <- function(x, y) {
+    is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)   
+}
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+### This function is very similar to the function from given example
+### The inverse_matrix stores computed inverse of matrix x
+    inverse_matrix <- NULL
+    set <- function(y){
+        ### If new matrix happens to be the same as old one, we don't have to compute inverse
+        if (!matequal(x,y)){
+            x <<- y
+            inverse_matrix <<- NULL    
+        }
+    }
+    get <- function() x
+    setinverse <- function(inverse) inverse_matrix <<- inverse
+    getinverse <- function() inverse_matrix
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    ### If inverse was already computed, we take it from "matrix" x
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
